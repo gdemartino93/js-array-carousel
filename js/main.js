e3778175e694146684bc3c6a019c89ed6877bc92
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2022-11-11 18:26:11 +0100

    array img inner

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
-console.log("asdasd")
+const imgArray=[
+`img/01.webp`,
+`img/02.webp`,
+`img/03.webp`,
+`img/04.webp`,
+`img/05.webp`,
+]
+
+let box=document.getElementById("output")
+// definisco container
+const mybtn=document.getElementById("mybtn")
+const arrowLeft=document.getElementById("arrow-left");
+const arrowRight=document.getElementById("arrow-right");
+// dEFinisco tasto e i due tag <a> arrow
+
+// inserisco le img
+for (let i = 0; i < imgArray.length; i++){
+ box.innerHTML+=`
+   <img src="${imgArray[i]}" alt="" class="d-none>`
+
+ 
+ 
+ 
+ 
+}
+
+mybtn.addEventListener("click",
+function(){
+}
+)
+
