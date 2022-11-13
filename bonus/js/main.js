60a486c506b62d86a5a74ff05d51cc4d774eaf37
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2022-11-13 21:02:54 +0100

    pointer scroll auto

--- bonus/js/main.js
+++ bonus/js/main.js
@@ -10,22 +10,69 @@ const imgArray=[
 let bigImg=document.getElementById("output");
 let minImg=document.getElementById("colonna");
 let attiva= 0;
-console.log(bigImg)
-console.log(minImg)
+let pointerAttiva=0;
+let pointercontainer=document.getElementById("pointer-container");
 // Dichiaro i due container delle img e puntatore
 
 for (let i = 0 ; i < imgArray.length ; i++) {
-  bigImg.innerHTML+=`<img src="${imgArray[i]}" alt="" class="none" id="img-${i}">`
-  minImg.innerHTML+=`<img src="${imgArray[i]}" alt="" class="none" id="min-${i}">`
+  bigImg.innerHTML+=`<img src="${imgArray[i]}" alt="" class="none" id="img-${i}">`;
+  minImg.innerHTML+=`<img src="${imgArray[i]}" alt="" class="" id="min-${i}">`
+  pointercontainer.innerHTML+=`<div class="pointer" id="nr-${i}"></div>`
+  var pointerUno=document.getElementById("nr-" + 0)
+  console.log(pointerUno)
 }
 // Inseriamo le img grandi e piccola
 
 let bigActive=document.getElementById("img-" + attiva)
 let minActive=document.getElementById("min-" + attiva)
-console.log(bigActive)
-console.log(minActive)
+
 
 bigActive.classList.add("block");
 minActive.classList.add("block");
-// Blocchiamo la priam img
+pointerUno.classList.add("pointer-active")
+// Blocchiamo la prima img
+
+let arrowUp= document.getElementById("arrow-up");
+let arrowDown=document.getElementById("arrow-down");
+// dichiaro frecce
+
+
+
+// dichiaro i pointer
+
+arrowUp.addEventListener("click", slideUp)
+arrowDown.addEventListener("click", slideDown)
+
+// dichiaro le funzioni al click
+
+function slideUp(){
+ attiva--
+ if (attiva < 0) {
+  attiva=imgArray.length - 1
+ }
+ bigActive.classList.remove("block");
+ minActive.classList.remove("active-colonna");
+ bigActive=document.getElementById("img-" + attiva);
+ minActive=document.getElementById("min-" + attiva);
+ bigActive.classList.add("block");
+ minActive.classList.add("active-colonna");
+ pointerUno.classList.remove("pointer-active")
+ pointerUno=document.getElementById("nr-" + attiva);
+ pointerUno.classList.add("pointer-active")
+}
+function slideDown(){
+  attiva++
+  if (attiva > ((imgArray.length)-1)) {
+   attiva=0
+  }
+  bigActive.classList.remove("block");
+  minActive.classList.remove("active-colonna");
+  bigActive=document.getElementById("img-" + attiva);
+  minActive=document.getElementById("min-" + attiva);
+  bigActive.classList.add("block");
+  minActive.classList.add("active-colonna");
+  pointerUno.classList.remove("pointer-active")
+  pointerUno=document.getElementById("nr-" + attiva);
+  pointerUno.classList.add("pointer-active")
+ }
 
